058bb9da59759ba797af2d303a41c10d5ebb86d7
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-11 13:13:22 -0300

    MUI config

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,9 @@
+import { Typography } from "@mui/material";
+
 export const JournalPage = () => {
   return (
-    <h1>JournalPage</h1>
-  )
-}
+  <>
+   <Typography variant='h1'>JournalPage</Typography>
+  </>
+  );
+};
