c13ba537c34270fb8fb95c76b7853da885bcdeab
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-16 03:55:27 -0300

    creates JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,17 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-  <>
-   <Typography variant='h1'>JournalPage</Typography>
-  </>
+    <JournalLayout>
+      <Typography>
+        Cupidatat aliquip ut veniam cupidatat cupidatat elit eu non aliqua
+        consectetur. Ea eu proident officia nulla ex labore occaecat ut velit
+        nulla minim id mollit. Ipsum aliquip nostrud minim qui cupidatat eiusmod
+        duis culpa. Ad aute exercitation est non irure eiusmod officia Lorem
+        adipisicing. Amet laborum sint ad do non labore nisi minim. Irure
+        consectetur veniam cillum et.
+      </Typography>
+    </JournalLayout>
   );
 };
