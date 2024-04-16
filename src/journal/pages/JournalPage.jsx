dbede1074b61b1b10831385345003e1cd222c809
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-16 05:03:16 -0300

    creates NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,17 +1,11 @@
-import { Typography } from "@mui/material";
+
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
-        Cupidatat aliquip ut veniam cupidatat cupidatat elit eu non aliqua
-        consectetur. Ea eu proident officia nulla ex labore occaecat ut velit
-        nulla minim id mollit. Ipsum aliquip nostrud minim qui cupidatat eiusmod
-        duis culpa. Ad aute exercitation est non irure eiusmod officia Lorem
-        adipisicing. Amet laborum sint ad do non labore nisi minim. Irure
-        consectetur veniam cillum et.
-      </Typography>
+      <NothingSelectedView />
     </JournalLayout>
   );
 };
