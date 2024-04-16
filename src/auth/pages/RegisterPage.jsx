60dc4eb83e2ef263099168b6f92a1d11bce8f527
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-16 03:47:28 -0300

    updates RegisterPage desing

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,47 @@
+import { Grid, Typography, TextField, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
   return (
-    <>
-      <h1>RegisterPage</h1>
-    </>
+    <AuthLayout title="Register">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Full Name"
+              type="text"
+              placeholder="Your name"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="example@gmail.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="Password"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container direction={"row"} justifyContent="end" sx={{ mt: 2 }}>
+            <Typography sx={{ mr: 1 }}>Do you have an account?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Login
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
   );
 };
