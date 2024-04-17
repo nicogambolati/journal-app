53a1e61ab82823e374db6a2172e4369de8f4c3e8
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-17 04:50:21 -0300

    register user form

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,25 @@
-import { Grid, Typography, TextField, Link } from "@mui/material";
+import { Grid, Typography, TextField, Link, Button } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+
+const formData = {
+  email: "nicolas@gmail.com",
+  password: "123456",
+  displayName: "Nicolas Gambolati",
+};
 
 export const RegisterPage = () => {
+  const { displayName, email, password, onInputChange, formState } = useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+
   return (
     <AuthLayout title="Register">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -13,6 +27,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Your name"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -22,6 +39,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="example@gmail.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -31,7 +51,18 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" fullWidth>
+                Create account
+              </Button>
+            </Grid>
           </Grid>
 
           <Grid container direction={"row"} justifyContent="end" sx={{ mt: 2 }}>
