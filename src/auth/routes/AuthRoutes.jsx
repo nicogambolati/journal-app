a8a6478fd77bbcc72d2c810728799b368c74880d
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-17 12:53:10 -0300

    creates CheckingAuth

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,13 +1,21 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { LoginPage, RegisterPage } from '../pages'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage, RegisterPage } from "../pages";
+import { useSelector } from "react-redux";
+import { CheckingAuth } from "../../ui/";
 
 export const AuthRoutes = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === "checking") {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
-      <Route path='login' element={<LoginPage />} />
-      <Route path='register' element={<RegisterPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
 
-      <Route path='/*' element={<Navigate to='/auth/login' />} />
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
-  )
-}
+  );
+};
