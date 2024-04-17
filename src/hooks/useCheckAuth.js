03aed14ebb04e45db387323de539ee018e5500d1
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-17 13:18:09 -0300

    creates useCheckAuth - custom hook

---
+++ src/hooks/useCheckAuth.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { login, logout } from "../store/auth";
+
+export const useCheckAuth = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) {
+        return dispatch(logout());
+      }
+
+      const { uid, displayName, email, photoURL } = user;
+
+      dispatch(
+        login({
+          uid: uid,
+          displayName: displayName,
+          email: email,
+          photoURL: photoURL,
+        })
+      );
+    });
+  }, []);
+
+  return  status ;
+};
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,36 +1,12 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
-import { FirebaseAuth } from "../firebase/config";
 
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { login, logout } from "../store/auth";
 import { CheckingAuth } from "../ui";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
-  const { status } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) {
-        return dispatch(logout());
-      }
-
-      const { uid, displayName, email, photoURL } = user;
-
-      dispatch(
-        login({
-          uid: uid,
-          displayName: displayName,
-          email: email,
-          photoURL: photoURL,
-        })
-      );
-    });
-  }, []);
+  const status = useCheckAuth();
 
   if (status === "checking") {
     return <CheckingAuth />;
