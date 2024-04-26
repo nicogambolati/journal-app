daa3d08f811b25f375d15fe47be585f2339c4343
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-26 07:21:27 -0300

    config environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,30 +6,23 @@ import { getEnvironments } from "../helpers";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// console.log(import.meta.env.VITE_HOLA);
-// console.log(process.env);
-const env = getEnvironments();
-console.log(env);
-
 // Your web app's Firebase configuration
-// Development / Production
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDlEv1tc31E9na0UQlnX-NjzWrouMmAspw",
-//   authDomain: "journal-app-react-8c96f.firebaseapp.com",
-//   projectId: "journal-app-react-8c96f",
-//   storageBucket: "journal-app-react-8c96f.appspot.com",
-//   messagingSenderId: "118478225432",
-//   appId: "1:118478225432:web:572be25461b7325197c36d",
-// };
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
 
-// Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyA8UtShcMpR8iWo3106EisfS4TEFFbWBzQ",
-  authDomain: "react-firebase-testing-87ade.firebaseapp.com",
-  projectId: "react-firebase-testing-87ade",
-  storageBucket: "react-firebase-testing-87ade.appspot.com",
-  messagingSenderId: "85036446041",
-  appId: "1:85036446041:web:2bd666ab617d9dc9a09c48",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
 
 // Initialize Firebase
